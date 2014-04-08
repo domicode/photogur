0d57b82a7eb3cc0ccd19e7eaa7406f60b47860f0
domicode <domi@domidesign.ch>
2014-04-08 13:12:24 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Photogur::Application.config.secret_key_base = '50e3753fb72b1b5c97d0e1df5dd75e9f34adb1d67a2e190b0838f2c283f0fa8293a46359fdb229d85321738ed117d2cb48b63b9c7f952f607b4bc7fc8d9caf84'
